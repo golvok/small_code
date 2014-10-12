5eb8e5a2af830d1f3e4e153f200c7c5c06f56851
Matthew J.P. Walker <walkerm930@gmail.com>
2014-10-12 15:17:27 -0400

    add {pop,remove}_any - remove/pop any node, including the tail and remove_by_value

--- C_stl/linked_list.c
+++ C_stl/linked_list.c
@@ -33,7 +33,9 @@ type##_ll_node_data* type##_ll_get_data(type##_ll_node* node) { return &ll_get_d
 bool type##_ll_is_empty(type##_linked_list* ll) { return ll_is_empty(ll); } \
 int type##_ll_insert_after(type##_linked_list* ll, type##_ll_node* node, type##_ll_node* new_next); \
 int type##_ll_remove_next(type##_linked_list* ll, type##_ll_node* node); \
+int type##_ll_remove_next_any(type##_linked_list* ll, type##_ll_node* prev_prev, type##_ll_node* prev); \
 int type##_ll_pop_next(type##_linked_list* ll, type##_ll_node* node, type##_ll_node_data* ret); \
+int type##_ll_pop_next_any(type##_linked_list* ll, type##_ll_node* prev_prev, type##_ll_node* prev, type##_ll_node_data* ret); \
  \
 type##_ll_node* type##_ll_node_init(type##_ll_node_data data); \
 void type##_ll_node_destroy(type##_ll_node* node); \
@@ -98,6 +100,19 @@ int type##_ll_remove_by_index(type##_linked_list* ll, int index, type##_ll_node_
 	} \
 } \
  \
+int type##_ll_remove_by_value(type##_linked_list* ll, type##_ll_node_data val) { \
+	if (ll_is_empty(ll)) { return 1; } \
+	type##_ll_node* prev_prev = 0; \
+	type##_ll_node* prev = ll_begin(ll); \
+	for (;ll_next(prev) != ll_end(ll); prev_prev = prev, ll_increment(prev)) { \
+		if (ll_get_data(ll_next(prev)) == val) { \
+			break; \
+		} \
+	} \
+	if (ll_end(ll) == ll_next(prev)) { return 1; } \
+	return type##_ll_remove_next_any(ll,prev_prev,prev); \
+} \
+ \
 int type##_ll_insert_after(type##_linked_list* ll, type##_ll_node* node, type##_ll_node* new_next) { \
 	if (new_next == 0 || node == 0) { return 1; } \
  \
@@ -111,26 +126,45 @@ int type##_ll_insert_after(type##_linked_list* ll, type##_ll_node* node, type##_
 	return 0; \
 } \
  \
-int type##_ll_remove_next(type##_linked_list* ll, type##_ll_node* node) { \
-	return type##_ll_pop_next(ll,node,0); \
+int type##_ll_remove_next(type##_linked_list* ll, type##_ll_node* prev) { \
+	return type##_ll_pop_next(ll,prev,0); \
+} \
+int type##_ll_remove_next_any(type##_linked_list* ll, type##_ll_node* prev_prev, type##_ll_node* prev) { \
+	return type##_ll_pop_next_any(ll,prev_prev,prev,0); \
 } \
  \
-int type##_ll_pop_next(type##_linked_list* ll, type##_ll_node* node, type##_ll_node_data* ret) { \
-	if (node == 0) { return 1; } \
+int type##_ll_pop_next(type##_linked_list* ll, type##_ll_node* prev, type##_ll_node_data* ret) { \
+	return type##_ll_pop_next_any(ll,0,prev,ret); \
+} \
  \
-	if (ll->pre_tail == node && ll->pre_head != ll->pre_tail) { \
-		assert(0 && "removing the tail is unsupported"); \
+int type##_ll_pop_next_any(type##_linked_list* ll, type##_ll_node* prev_prev, type##_ll_node* prev, type##_ll_node_data* ret) { \
+	if (prev == 0) { return 1; } \
+ \
+	if (ll->pre_tail == prev && ll->pre_head != ll->pre_tail && prev_prev == 0) { \
+		assert(0 && "removing the tail without prev_prev is unsupported"); \
 	} \
  \
-	type##_ll_node* old_next = ll_next(node); \
+	if (prev != ll->pre_head) { \
+		if (prev_prev == 0) { \
+			if (ll->pre_tail != prev) { \
+				assert(0 && "need the prev_prev to remove the tail"); \
+			} \
+		} else if (ll_next(prev_prev) != prev) { \
+			assert(0 && "prev_prev isn't the node before prev"); \
+		} \
+	} \
+ \
+	type##_ll_node* old_next = ll_next(prev); \
 	if (old_next == 0) { return 1; } \
 	 \
-	ll_next(node) = ll_next(old_next); \
+	ll_next(prev) = ll_next(old_next); \
 	if (ret != 0) { \
 		*ret = *type##_ll_get_data(old_next); \
 	} \
 	if (ll->pre_tail == old_next) { \
-		ll->pre_tail = node; \
+		ll->pre_tail = prev; \
+	} else if (ll->pre_tail == prev && !ll_is_empty(ll)) { \
+		ll->pre_tail = prev_prev; \
 	} \
 	type##_ll_node_destroy(old_next); \
 	return 0; \
@@ -216,6 +250,11 @@ int main() {
 			float_ll_push_back(ll, float_ll_node_init(i));
 		}
 		
+		PRINT_LL(float,ll);
+
+		puts("removing 11.0!");
+		float_ll_remove_by_value(ll,11);
+
 		PRINT_LL(float,ll);
 
 		while (!float_ll_is_empty(ll)) {
