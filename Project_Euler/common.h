c7a4d4121341c8e615ee1181b072f1cf6779a1cb
Matthew J.P. Walker <walkerm930@gmail.com>
2015-01-05 22:14:31 -0500

    do PE #82

--- Project_Euler/common.h
+++ Project_Euler/common.h
@@ -58,7 +58,7 @@ typename TwoDeeArrayType::value_type::reference arrayGet(TwoDeeArrayType& a, Poi
 }
 
 template<typename TwoDeeArrayType>
-typename TwoDeeArrayType::value_type::value_type arrayGet(const TwoDeeArrayType& a, Point location) {
+const typename TwoDeeArrayType::value_type::reference arrayGet(const TwoDeeArrayType& a, Point location) {
 	return a[location.second][location.first];
 }
 
@@ -79,4 +79,89 @@ void combineInto(InputIter first1, InputIter last1, InOutIter first2, BinaryOp o
 template<typename InputIter, typename InOutIter>
 void addTo(InputIter first1, InputIter last1, InOutIter first2) {
 	combineInto(first1,last1,first2, std::plus< typename std::remove_reference<decltype(*first1)>::type>());
+}
+
+template<typename TwoDeeArrayType>
+class ColIter {
+public:
+	typedef std::forward_iterator_tag iterator_category;
+	typedef typename TwoDeeArrayType::value_type::value_type value_type;
+	typedef typename TwoDeeArrayType::difference_type difference_type;
+	typedef value_type* pointer;
+	typedef value_type& reference;
+
+	typedef typename TwoDeeArrayType::value_type::size_type col_size_type;
+	typedef typename TwoDeeArrayType::size_type row_size_type;
+
+	static const col_size_type END_VAL = -1;
+
+	ColIter(TwoDeeArrayType& backing_array_, col_size_type column_index_, row_size_type start_row = END_VAL)
+		: backing_array(backing_array_)
+		, column_index(column_index_)
+		, current_index(start_row)
+		{}
+
+	ColIter(const ColIter& src)
+		: backing_array(src.backing_array)
+		, column_index(src.column_index)
+		, current_index(src.current_index)
+		{}
+
+	value_type& operator*() {
+		return backing_array[current_index][column_index];
+	}
+
+	template<typename IntType>
+	ColIter operator+(IntType delta) {
+		ColIter result(*this);
+		result += delta;
+		return result;
+	}
+
+	difference_type operator-(const ColIter& other) const {
+		return this->current_index - other.current_index;
+	}
+
+	template<typename IntType>
+	ColIter& operator+=(IntType delta) {
+		if(current_index != END_VAL) {
+			current_index += delta;
+			if (current_index >= backing_array.size()) {
+				current_index = END_VAL;
+			}
+		}
+		return *this;
+	}
+
+	ColIter& operator++() {
+		*this += 1;
+		return *this;
+	}
+
+	bool operator==(const ColIter& other) {
+		return
+			(&backing_array == &other.backing_array)
+			&& (column_index == other.column_index)
+			&& (current_index == other.current_index)
+		;
+	}
+
+	bool operator!=(const ColIter& other) {
+		return !(*this == other);
+	}
+
+private:
+	TwoDeeArrayType& backing_array;
+	col_size_type column_index;
+	row_size_type current_index;
+};
+
+template<typename TwoDeeArrayType>
+ColIter<TwoDeeArrayType> make_col_iter(TwoDeeArrayType& backing_array, size_t column_index) {
+	return ColIter<TwoDeeArrayType>(backing_array, column_index);
+}
+
+template<typename TwoDeeArrayType>
+ColIter<TwoDeeArrayType> make_col_iter(TwoDeeArrayType& backing_array, size_t column_index, size_t start_row) {
+	return ColIter<TwoDeeArrayType>(backing_array, column_index, start_row);
 }
